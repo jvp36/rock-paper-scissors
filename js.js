4f2ef9ea2c1e9dbf1cd05786a671e1ff77b4f64f
jvp36 <jvp.36@yahoo.com>
2024-11-30 14:05:51 -0400

    Add getHumanChoice function

--- js.js
+++ js.js
@@ -6,4 +6,19 @@ const getComputerChoice = function () {
     return options[num];
 }
 
+// get human choice
+
+const getHumanChoice = function () {
+    let condition = true;
+    while (condition) {
+        let humaChoice = prompt ("Write rock, paper or scissors? ").toLowerCase();
+        if (humaChoice.trim() === "rock" || humaChoice.trim() === "paper" || humaChoice.trim() === "scissors") {
+            condition = false;
+            return humaChoice.trim();
+        } else {
+            alert("You must write only one of the three options");
+        }        
+    }    
+}
 console.log(getComputerChoice());
+console.log(getHumanChoice());
