a381d793f62450c5588ee0d2f0a8f35f5fc515ad
jvp36 <jvp.36@yahoo.com>
2024-11-30 18:34:36 -0400

    Add play round function

--- js.js
+++ js.js
@@ -1,3 +1,7 @@
+// variables
+
+let humanScore = 0;
+let computerScore = 0;
 // get computer choice
 
 const getComputerChoice = function () {
@@ -11,14 +15,54 @@ const getComputerChoice = function () {
 const getHumanChoice = function () {
     let condition = true;
     while (condition) {
-        let humaChoice = prompt ("Write rock, paper or scissors? ").toLowerCase();
-        if (humaChoice.trim() === "rock" || humaChoice.trim() === "paper" || humaChoice.trim() === "scissors") {
+        let humanChoice = prompt ("Write rock, paper or scissors? ").toLowerCase();
+        if (humanChoice.trim() === "rock" || humanChoice.trim() === "paper" || humanChoice.trim() === "scissors") {
             condition = false;
-            return humaChoice.trim();
+            return humanChoice.trim();
         } else {
             alert("You must write only one of the three options");
         }        
     }    
 }
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+
+// play round function
+
+const playRound = function (humanChoice, computerChoice) {
+    computerChoice = getComputerChoice();
+    humanChoice = getHumanChoice();
+    if (humanChoice === "rock") {
+        if (computerChoice === "rock") {
+            return "You chose rock. Computer chose rock. This round is tied!"
+        } else if (computerChoice === "paper") {
+            computerScore += 1;
+            return "You chose rock. Computer chose paper. You lost! Paper beats rock."
+        } else {
+            humanScore += 1;
+            return "You chose rock. Computer chose scissors. You won! Rock beats scissors."
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            humanScore += 1;
+            return "You chose paper. Computer chose rock. You won! Paper beats rock."
+        } else if (computerChoice === "paper") {
+            return "You chose paper. Computer chose paper. This round is tied!"
+        } else {
+            computerScore += 1;
+            return "You chose paper. Computer chose scissors. You lost! Scissors beats paper."
+        }
+    } else {
+        if (computerChoice === "rock") {
+            computerScore += 1;
+            return "You chose scissors. Computer chose rock. You lost! Rock beats scissors."
+        } else if (computerChoice === "paper") {
+            humanScore += 1;
+            return "You chose scissors. Computer chose paper. You won! Scissors beats paper."
+        } else {
+            return "You chose scissors. Computer chose scissors. This round is tied!"
+        }
+    }
+}
+
+console.log(playRound());
+console.log("You won:", humanScore);
+console.log("You lost:", computerScore);
